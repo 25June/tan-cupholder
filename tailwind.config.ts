2ffbcc306e2cab1ab32b8a38fb875c23794d8803
phunguyen <ngocphu2506@gmail.com>
2025-02-13 22:30:44 +0700

    setup project

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out', // Adjust duration and easing
+        'fade-out': 'fadeOut 0.5s ease-in-out', // Adjust duration and easing
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        },
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
