458ed0728158f154af34498d85afbf4cc55d536a
phunguyen <ngocphu2506@gmail.com>
2025-03-17 23:42:30 +0700

    fix issue

--- src/components/product-slider/ProductSlider.tsx
+++ src/components/product-slider/ProductSlider.tsx
@@ -37,7 +37,7 @@ const mockData = [
 
 export const ProductSlider = () => {
   const numberWithCommas = (x: number) => {
-    let parts = x.toString().split('.');
+    const parts = x.toString().split('.');
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
     return parts.join(',');
   };
