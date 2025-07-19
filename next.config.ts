fd0ade111e8f80c4cae1990b11473a680f4f7f4a
phunguyen <ngocphu2506@gmail.com>
2025-07-19 13:11:42 +0700

    test husky

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-485637738840450490e408cee2acb72c.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
