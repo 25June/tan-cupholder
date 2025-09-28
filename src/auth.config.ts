9454b07302c5e0e508d946cc99494938c5f389d2
phunguyen <ngocphu2506@gmail.com>
2025-09-28 21:34:54 +0700

    update secret

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,8 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
+  session: { strategy: 'jwt' },
+  secret: process.env.AUTH_SECRET,
   pages: {
     signIn: '/admin'
   },
